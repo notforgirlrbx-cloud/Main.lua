64f9c45bf16a31174391ccd3dd69dd75710a7ef7
notforgirlrbx-cloud <notforgirlrbx@gmail.com>
2026-05-14 18:50:55 +0630

    Redziscoming
    Kk

--- Main.lua
+++ Main.lua
@@ -75,6 +75,33 @@ MainTab:AddButton({
   end
 })
 
+MainTab:AddButton({
+  Name = "Infinite Jump Toggle",
+   Callback = function()
+       --Toggles the infinite jump between on or off on every script run
+_G.infinjump = not _G.infinjump
+
+if _G.infinJumpStarted == nil then
+	--Ensures this only runs once to save resources
+	_G.infinJumpStarted = true
+	
+	game.StarterGui:SetCore("SendNotification", {Title="Youtube Hub"; Text="Infinite Jump Activated!"; Duration=5;})
+
+	local plr = game:GetService('Players').LocalPlayer
+	local m = plr:GetMouse()
+	m.KeyDown:connect(function(k)
+		if _G.infinjump then
+			if k:byte() == 32 then
+			humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+			humanoid:ChangeState('Jumping')
+			wait()
+			humanoid:ChangeState('Seated')
+			end
+		end
+	end)
+end
+   end
+})
 
 MainTab:AddSection("Toggle")
 MainTab:AddToggle({
